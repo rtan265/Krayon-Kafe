d81bce1b6c3c650d5769dba1c43be89e851b3653
jemimahmartinez <martinezjemimah@gmail.com>
2020-08-01 16:12:44 +1200

    set up frontend

--- index.js
+++ index.js
@@ -2,7 +2,31 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.sendfile("index.html"));
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// index page 
+// app.get('/', function(req, res) {
+//   res.render('pages/index');
+// });
+
+app.get('/', function(req, res) {
+  var drinks = [
+      { name: 'Bloody Mary', drunkness: 3 },
+      { name: 'Martini', drunkness: 5 },
+      { name: 'Scotch', drunkness: 10 }
+  ];
+  var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
+
+  res.render('pages/index', {
+      drinks: drinks,
+      tagline: tagline
+  });
+});
+
+app.get('/map', function(req,res) {
+  res.render('pages/map');
+});
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
